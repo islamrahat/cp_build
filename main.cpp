0de07541b55e4c91ee74b3c7148fd93c63e92962
islamrahat <168159406+islamrahat@users.noreply.github.com>
2024-12-25 19:41:23 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#define inparr(n, arr) for(int i=0; i<n; i++){scanf("%d", &arr[i]);}
+#define outparr(n, arr) for(int i=0; i<n; i++){printf("%d ", arr[i]);}
+int max(int n, int arr[]) {int max;for(int i=0; i<n; i++){if(arr[i]>max) max = arr[i];}return max;}
+#define sortasc(n, arr) for(int i=0; i<n; i++){for(int j=0; j<n-1; j++){if(arr[j]>arr[j+1]){int temp = arr[j];arr[j] = arr[j+1];arr[j+1] = temp;}}}
+#define sortdesc(n, arr) for(int i=0; i<n; i++){for(int j=0; j<n-1; j++){if(arr[j]<arr[j+1]){int temp = arr[j];arr[j] = arr[j+1];arr[j+1] = temp;}}}
+
+int main()
+{
+    int test;
+    scanf("%d", &test);
+    while(test--)
+    {
+        
+    }
+    return 0;
+}
