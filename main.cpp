7bb406d029fcef0e308ccfe4375b1d1a6394d62f
islamrahat <168159406+islamrahat@users.noreply.github.com>
2024-12-25 19:43:54 +0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #define inparr(n, arr) for(int i=0; i<n; i++){scanf("%d", &arr[i]);}
 #define outparr(n, arr) for(int i=0; i<n; i++){printf("%d ", arr[i]);}
 int max(int n, int arr[]) {int max;for(int i=0; i<n; i++){if(arr[i]>max) max = arr[i];}return max;}
+int min(int n, int arr[]) {int min;for(int i=0; i<n; i++){if(arr[i]<min) min = arr[i];}return min;}
 #define sortasc(n, arr) for(int i=0; i<n; i++){for(int j=0; j<n-1; j++){if(arr[j]>arr[j+1]){int temp = arr[j];arr[j] = arr[j+1];arr[j+1] = temp;}}}
 #define sortdesc(n, arr) for(int i=0; i<n; i++){for(int j=0; j<n-1; j++){if(arr[j]<arr[j+1]){int temp = arr[j];arr[j] = arr[j+1];arr[j+1] = temp;}}}
 
